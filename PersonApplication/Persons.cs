c7c7ae09a9c1d9e4aa5f9105f8ae63fd332bfd37
K8241 <K8241@student.jamk.fi>
2017-02-15 12:16:43 +0200

    Demo05

---
+++ PersonApplication/Persons.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApplication
+{
+    /// <summary>
+    /// This class holds person objects in a collection.
+    /// </summary>
+    class Persons
+    {
+        /// <summary>
+        /// Collection to store person objects
+        /// </summary>
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor initilizes a collection
+        /// </summary>
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+
+        /// <summary>
+        /// Add person to collection
+        /// </summary>
+        /// <param name="person">Person to add</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        /// <summary>
+        /// Returns person to caller application
+        /// </summary>
+        /// <param name="index">index in collection</param>
+        /// <returns>person objects or null</returns>
+
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count)
+                return persons.ElementAt(index);
+            else return null;
+        }
+
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    return person;
+                }
+            }
+            // no person with that socialsecuritynumber...
+            return null;
+        }
+
+        public void PrintCollection()
+        {
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
--- PersonApplication/Program.cs
+++ PersonApplication/Program.cs
@@ -10,6 +10,49 @@ namespace PersonApplication
     {
         static void Main(string[] args)
         {
+            //create a few person objects
+
+
+            Person person1 = new Person { Firstname = "Kirsi", Lastname = "Kekkonen", SocialSecurityNumber = "947363-344" };
+
+            Person person2 = new Person { Firstname = "Pekka", Lastname = "Kallio", SocialSecurityNumber = "403099-393" };
+
+            Person person3 = new Person { Firstname = "Milla", Lastname = "Meilaaja", SocialSecurityNumber = "5555-55-5-5" };
+
+            // create a Persons object
+
+            Persons myfriens = new Persons();
+
+            //add person objects to persons collection
+            myfriens.AddPerson(person1);
+            myfriens.AddPerson(person2);
+            myfriens.AddPerson(person3);
+
+            //get one person from Persons collection
+            Person person4 = myfriens.GetPerson(100);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("Person not found in that index!");
+            }
+
+            //print collection
+            myfriens.PrintCollection();
+
+            //find person
+            string sotu = "403099-33";
+            Console.WriteLine("Find sotu: " + sotu);
+            Person person5 = myfriens.FindPerson(sotu);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            } else
+            {
+                Console.WriteLine("Cant find person with that socialsecuritynumber");
+            }
+
         }
     }
 }
